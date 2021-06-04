242bd96432ab05a9fb42259fc8f863eac776285a
Felipe Botero <felipe.botero.botero@gmail.com>
2021-06-03 22:41:17 -0400

    added update and delete

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+
 from .models import Posts
+
 
 class BlogListView(ListView):
     model = Posts
@@ -9,3 +17,17 @@ class BlogDetailView(DetailView):
     model = Posts
     template_name = "post_detail.html"
 
+class BlogCreateView(CreateView):
+    model = Posts
+    template_name = "post_new.html"
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Posts
+    template_name = "post_edit.html"
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Posts
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')

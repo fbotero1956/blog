9e3e76b7160d09017192dfc38c0e043b21680921
Felipe Botero <felipe.botero.botero@gmail.com>
2021-06-03 21:07:20 -0400

    added tests

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Posts
 
 class BlogListView(ListView):
     model = Posts
     template_name = "home.html"
 
-
+class BlogDetailView(DetailView):
+    model = Posts
+    template_name = "post_detail.html"
 
